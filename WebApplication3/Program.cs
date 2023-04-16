59c2f91795f5699870011f3c3e25fb6bb313eecb
Gabriel Enström <gabriel.enstrom@qrew.se>
2023-04-16 18:24:52 +0200

    feat: Added querytype

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -1,5 +1,12 @@
+using WebApplication3.Types.BookType;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<BookQuery>();
 var app = builder.Build();
+app.MapGraphQL();
 
 app.MapGet("/", () => "Hello World!");
 
